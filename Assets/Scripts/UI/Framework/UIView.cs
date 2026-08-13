0c56cc4908a2d402968e36268711fbd49c4c9689
Nero-UMU <2572591652@qq.com>
2026-08-13 14:43:18 +0800

    添加UI组件

---
+++ Assets/Scripts/UI/Framework/UIView.cs
@@ -0,0 +1,44 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public abstract class UIView : MonoBehaviour
+{
+    [SerializeField] private UILayer layer = UILayer.Normal;
+
+    public UILayer Layer => layer;
+    public bool IsOpen { get; private set; }
+    public event Action<UIView> Closed;
+    public void Open(object param)
+    {
+        if (IsOpen) return;
+        IsOpen = true;
+        gameObject.SetActive(true);
+        OnOpen(param);
+    }
+
+    public void Close()
+    {
+        if (!IsOpen) return;
+        OnClose();
+        IsOpen = false;
+        gameObject.SetActive(false);
+        Closed?.Invoke(this);
+    }
+
+    protected void CloseSelf()
+    {
+        UIManager.instance.Close(this);
+    }
+
+    protected virtual void OnOpen(object param) { }
+    protected virtual void OnClose() { }
+}
+
+public enum UILayer
+{
+    Normal = 0,
+    Popup = 1,
+    System = 2
+}
