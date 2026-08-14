c0d43a7b37cb8934b1f1e8c48babccc5160d0864
木卫二超人 <1186947170@qq.com>
2026-08-14 16:49:36 +0800

    feat(battle): 调整回合结算时序并搭建遗物基础结构
    - 分离玩家与敌方卡牌行动结算
    - 调整时钟推进及双方护甲清空时机
    - 预留双方遗物触发节点
    - 添加随机获取存活卡牌的方法
    - 记录最大生命值与治疗上限待办
    - 创建了一个遗物

---
+++ Assets/Scripts/Card/Relic/IRelic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRelic
+{
+    CardMaterialData MaterialData { get; }
+    bool OnBattleStart(BattleManager battleManager,bool isEnemy);
+    bool OnRoundStart(BattleManager battleManager, bool isEnemy);
+    bool OnCardAction(BattleManager battleManager, bool isEnemy,CardEntity actingCard);
+}
---
+++ Assets/Scripts/Card/Relic/PorkLegRelic.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PorkLegRelic", menuName = "ScriptableObject/素材数据/遗物/PorkLegRelic")]
+[System.Serializable]
+public class PorkLegRelic:CardMaterialData,IRelic
+{
+    [SerializeField] int defence = 4;
+    public CardMaterialData MaterialData => this;
+    public bool OnBattleStart(BattleManager battleManager,bool isEnemy)
+    {
+        CardEntity entity = battleManager.GetRandomLivingCard(isEnemy);
+        if(entity == null)
+            return false;
+        entity.Defence(defence);
+        return true;
+    }
+    public bool OnRoundStart(BattleManager battleManager, bool isEnemy)
+    {
+        return false;
+    }
+    public bool OnCardAction(BattleManager battleManager, bool isEnemy, CardEntity actingCard)
+    {
+        return false;
+    }
+}
