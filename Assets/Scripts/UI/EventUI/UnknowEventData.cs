7ee13d66fd16bf956e18ae768182d664c7b8e9f4
NoHurry <1074172129@qq.com>
2026-08-14 23:20:08 +0800

    事件根据选项变为不同文本

--- Assets/Scripts/UI/EventUI/UnknowEventData.cs
+++ Assets/Scripts/UI/EventUI/UnknowEventData.cs
@@ -1,25 +1,45 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// δ֪�¼�����
+/// 未知事件数据
 /// </summary>
-public class UnknowEventData
+[CreateAssetMenu(fileName = "EventData", menuName = "ScriptableObject/事件数据", order = 0)]
+public class UnknowEventData : ScriptableObject
 {
-    public Text UnknowEventName; //�¼�����
-    public Text UnknowEventIntroduct; //�¼�����
-    public Image UnknowEventIcon; //�¼�ͼƬ
-    public List<UnknowEventOptionData> UnknowEventOptions; //�¼�ѡ�ť
+    public int UnknowEventID; //事件id
+    public string UnknowEventName; //事件名称
+    [TextArea(3,8)]
+    public string UnknowEventContent; //事件具体内容
+    public Sprite UnknowEventIcon; //事件图片
+    public List<UnknowEventOptionData> UnknowEventOptions; //事件选项按钮
 }
 
 /// <summary>
-/// δ֪�¼�����ѡ��
+/// 未知事件单个选项
 /// </summary>
+[System.Serializable]
 public class UnknowEventOptionData
 {
-    public string optionName; //ѡ������
-    public Action optionClicked;  //ѡ�����¼�
+    public string optionName; //选项文字
+
+    public UnknowEventActionType actionType;  //选项点击执行逻辑类型
+    public int actionValue; // 逻辑数值
+    public string actionText; // 道具名/音效名..
+
+    public UnknowEventData nextEventData; //下一个事件数据
+}
+
+/// <summary>
+/// 选项触发的逻辑类型
+/// </summary>
+[System.Serializable]
+public enum UnknowEventActionType
+{
+    None = 0,
+    //待添加
+
 }
